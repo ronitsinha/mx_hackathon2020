9fd400a09421f021ee7923ad1a3374b1b1faaa4c
NuoWenLei <46566976+NuoWenLei@users.noreply.github.com>
2020-05-03 15:47:27 -0400

    Made get_nearby_places a callable function

--- Backend/Store_Match_Places/nearby_places.py
+++ Backend/Store_Match_Places/nearby_places.py
@@ -11,35 +11,29 @@ API_KEY = 'AIzaSyA950W2SYIsCt6uvuFNItJmRGetaxY4W30'
 
 gmaps = googlemaps.Client(key = API_KEY)
 
-places_result = gmaps.places_nearby(location=lat_lon, radius=40000, type='restaurant')
 
-time.sleep(3)
+def get_nearby_places(placeId, serviceType, radius=24000 ):
+    center = gmaps.place(place_id=placeId)
+    places_result = gmaps.places_nearby(location=center['result']['geometry']['location'], radius=radius, type=serviceType)
+    stored_results = []
+    while len(places_result['results']) >= 20 and len(stored_results) <= 50:
+        for place in places_result['results']:
+            # define the place id, needed to get place details. Formatted as a string.
+            my_place_id = place['place_id']
+            # define the fields you would liked return. Formatted as a list.
+            my_fields = ['name', 'formatted_phone_number', 'formatted_address', 'website', 'place_id']
+            # make a request for the details.
+            places_details  = gmaps.place(place_id= my_place_id , fields= my_fields)
+            stored_results.append(places_details['result'])
+        time.sleep(1)
+        if len(stored_results) < 40:
+            places_result  = gmaps.places_nearby(page_token = places_result['next_page_token'])
+        
+    # stored_results return as list of dictionaries each with 
+    # name, phone number, address, website, and place_id
+    return stored_results
+#test
+#result = get_nearby_places(placeId='ChIJ80C0mkCa44kR4pqBL_NclhA', serviceType='restaurant' )
+#print(result)
+    
 
-place_result  = gmaps.places_nearby(page_token = places_result['next_page_token'])
-
-stored_results = []
-
-# loop through each of the places in the results, and get the place details.      
-for place in places_result['results']:
-
-    # define the place id, needed to get place details. Formatted as a string.
-    my_place_id = place['place_id']
-
-    # define the fields you would liked return. Formatted as a list.
-    my_fields = ['name','formatted_phone_number','website', 'adr_address']
-
-    # make a request for the details.
-    places_details  = gmaps.place(place_id= my_place_id , fields= my_fields)
-
-    # print the results of the details, returned as a dictionary.
-    pprint.pprint(places_details['result'])
-
-    # make an html2text object, convert html address to readable text
-    readable_address = html2text.HTML2Text().handle(places_details['result'].get("adr_address"))
-
-    # print readable address
-    print(readable_address)
-
-    # store the results in a list object.
-    stored_results.append(places_details['result'])
-    stored_results.append(readable_address)
