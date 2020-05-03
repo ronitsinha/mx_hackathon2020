7aedbd655e8eace0e5b243608a212955123ac6fa
ronitsinha <ronittheawesome@gmail.com>
2020-05-03 15:58:41 -0400

    Nuo Wen

--- app.py
+++ app.py
@@ -1,6 +1,44 @@
 import flask as fl
 import json
 import googlemaps
+import googlemaps
+import pprint
+import time
+import html2text
+# from GoogleMapsAPIKey import get_my_key
+
+lat_lon = '-33.8670522,151.1957362'
+
+#define API
+API_KEY = 'AIzaSyA950W2SYIsCt6uvuFNItJmRGetaxY4W30'
+
+gmaps = googlemaps.Client(key = API_KEY)
+
+
+def get_nearby_places(placeId, serviceType, radius=24000 ):
+    center = gmaps.place(place_id=placeId)
+    places_result = gmaps.places_nearby(location=center['result']['geometry']['location'], radius=radius, type=serviceType)
+    stored_results = []
+    while len(places_result['results']) >= 20 and len(stored_results) <= 50:
+        for place in places_result['results']:
+            # define the place id, needed to get place details. Formatted as a string.
+            my_place_id = place['place_id']
+            # define the fields you would liked return. Formatted as a list.
+            my_fields = ['name', 'formatted_phone_number', 'formatted_address', 'website', 'place_id']
+            # make a request for the details.
+            places_details  = gmaps.place(place_id= my_place_id , fields= my_fields)
+            stored_results.append(places_details['result'])
+        time.sleep(1)
+        if len(stored_results) < 40:
+            places_result  = gmaps.places_nearby(page_token = places_result['next_page_token'])
+        
+    # stored_results return as list of dictionaries each with 
+    # name, phone number, address, website, and place_id
+    return stored_results
+#test
+#result = get_nearby_places(placeId='ChIJ80C0mkCa44kR4pqBL_NclhA', serviceType='restaurant' )
+#print(result)
+    
 
 app = fl.Flask(__name__, static_folder='static')
 
@@ -22,20 +60,18 @@ def foodbank (name=None):
 def handle_foodbank_request ():
 	data = json.loads(fl.request.form['js_data'])
 
-	name = data['name']
-	location = data['location']
-	email = data['email']
-	phone = data['phone']
-	
-
 	app.logger.debug(data)
 
-	return jsonify({'name' : name, 'location' : location, 'email' : email, 'phone' : phone})
+	return ''
 
 @app.route('/handle_location/<user_type>', methods=['POST'])
 def handle_location (user_type=None):
 	location = fl.request.form['location'] 
 
-	name = 'pep'
+	
+
+	service_type = 'restaurant' if user_type=='foodbank' else 'foodbank'
+
+	nearby = get_nearby_places(location, user_type, 50)
 
 	return fl.redirect(fl.url_for(user_type, name=name))	
